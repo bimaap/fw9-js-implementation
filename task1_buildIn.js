2f5b9255578f43d90be84b211cff278159444df2
bimaap <let.bimaap@gmail.com>
2022-06-22 17:51:21 +0700

    fix: error lower case

--- task1_buildIn.js
+++ task1_buildIn.js
@@ -3,7 +3,7 @@ let text = 'malam'
 let hasil = []
 if(typeof(text) == 'string'){
     hasil = text.split('').reverse().join('')
-    if(text.toLocaleLowerCase === hasil.toLocaleLowerCase){
+    if(text.toLocaleLowerCase() === hasil.toLocaleLowerCase()){
         console.log('Palindrom');
     }else{
         console.log('Bukan Palindrom');
